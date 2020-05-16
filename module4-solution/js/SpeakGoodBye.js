07f1125346769c2e9d0aa2dccabf5e5d34a34d16
David Petersen <dpete106@gmail.com>
2020-05-16 10:53:19 -0400

    module 4 solution

---
+++ module4-solution/js/SpeakGoodBye.js
@@ -0,0 +1,19 @@
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+(function () {
+
+
+
+	var byeSpeaker = {};
+
+	var speakWord = "Good Bye";
+
+
+	byeSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+
+	window.byeSpeaker = byeSpeaker;
+})();
